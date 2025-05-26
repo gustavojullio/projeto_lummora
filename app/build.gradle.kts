5d282134d89ab9d725808f89342846279e602d2f
PMoraizz <pedroamc720@gmail.com>
2025-05-26 20:49:19 -0300

    Commit 26/05/25
    Criação de novas telas, e ajustes nas outras

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.projeto_lummora"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.projeto_lummora"
