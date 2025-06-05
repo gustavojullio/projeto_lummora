6a3a1881f327815ccf14c256e6f942ca95cdcd2f
Daniel.java <daniel.scarvalho05@gmail.com>
2025-06-05 10:00:24 -0300

    grafico
    grafico de materiais

--- app/src/main/java/com/example/projeto_lummora/Insights.java
+++ app/src/main/java/com/example/projeto_lummora/Insights.java
@@ -1,14 +1,26 @@
 package com.example.projeto_lummora;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class Insights extends AppCompatActivity {
+
+    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,40 @@ public class Insights extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_insights);
 
+        pieChart = (PieChart) findViewById(R.id.graficoMaterias);
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(1, 1, 1, 1);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleRadius(40f);
+        pieChart.setHoleColor(ContextCompat.getColor(this, R.color.YInMn_Blue));
+        pieChart.setTransparentCircleColor(ContextCompat.getColor(this, R.color.YInMn_Blue));
+        pieChart.setTransparentCircleRadius(45f);
+
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+
+        yValues.add(new PieEntry(34f, "A"));
+        yValues.add(new PieEntry(23f, "B"));
+        yValues.add(new PieEntry(14f, "C"));
+        yValues.add(new PieEntry(35, "D"));
+        yValues.add(new PieEntry(40, "E"));
+        yValues.add(new PieEntry(23, "F"));
+
+        PieDataSet dataSet = new PieDataSet(yValues, "");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(Cores.CORES_GRAFICO);
+
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setData(data);
+
     }
+
 }
