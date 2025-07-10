c58c794ba9ea0c4056a1e8b2fa5f81f4dd8f959a
PMoraizz <pedroamc720@gmail.com>
2025-07-10 08:26:11 -0300

    config usuario
    config usuario

--- app/src/main/java/com/example/projeto_lummora/ConfiguracoesConta.java
+++ app/src/main/java/com/example/projeto_lummora/ConfiguracoesConta.java
@@ -1,22 +1,18 @@
 package com.example.projeto_lummora;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class ConfiguracoesUsuario extends AppCompatActivity {
+public class ConfiguracoesConta extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_configuracoes_usuario);
+        setContentView(R.layout.activity_configuracoes_conta);
 
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
@@ -27,12 +23,11 @@ public class ConfiguracoesUsuario extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
         );
-
-
-
     }
-    public void onConfiguracoes(View v) {
-        Intent intent = new Intent(ConfiguracoesUsuario.this, ConfiguracoesConta.class);
-        startActivity(intent);
+
+
+
+    public void onVoltar(View view) {
+        finish();
     }
 }
